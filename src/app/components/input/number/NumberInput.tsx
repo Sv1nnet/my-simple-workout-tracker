d490896776c9bf45db9f8e109bac96131013a034
Leonid Tsukanov <sv1nnet1992@gmail.com>
2022-08-29 11:38:43 +0300

    NumberInput reworked; max 10 round added;

--- src/app/components/input/number/NumberInput.tsx
+++ src/app/components/input/number/NumberInput.tsx
@@ -1,64 +1,220 @@
 import { Input, InputProps } from 'antd'
-import { FC, useEffect, useState } from 'react'
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
-const NEG_FLOAT_REGEXP = /(^\d+$)|(^\d{1,}(\.|,)$)|(^\d+(\.|,)\d{1,}$)|(^(\.|,)\d{1,}$)/
-const POS_FLOAT_REGEXP = /(^\d+$)|(^\d{1,}(\.|,)$)|(^\d+(\.|,)\d{1,}$)|(^(\.|,)\d{1,}$)/
-const NEG_INT_REGEXP = /^-\d+$/
-const POS_INT_REGEXP = /^\d+$/
+const isPos = value => /^(?![-])/.test(value)
+const isZero = value => +value === 0
 
-const isInt = value => NEG_INT_REGEXP.test(value) || POS_INT_REGEXP.test(value)
-const isPosInt = value => POS_INT_REGEXP.test(value)
-const isNegInt = value => NEG_INT_REGEXP.test(value)
+const INT_REGEX = /(^\d+$)|(^-\d+$)/
+const isInt = value => (value !== '-' ? INT_REGEX.test(value) : true)
+const isPosInt = value => isInt(value) && isPos(value)
+const isNegInt = value => value !== '-' ? isInt(value) && (isZero(value) || !isPos(value)) : true
 
-const isFloat = value => NEG_FLOAT_REGEXP.test(value) || POS_INT_REGEXP.test(value)
-const isPosFloat = value => POS_FLOAT_REGEXP.test(value)
-const isNegFloat = value => NEG_FLOAT_REGEXP.test(value)
+const FLOAT_REGEX = /(^\d+$)|(^\d{1,}(\.|,)$)|(^\d+(\.|,)\d{1,}$)|(^(\.|,)\d{1,}$)|(^-\d+$)|(^-\d{1,}(\.|,)$)|(^-\d+(\.|,)\d{1,}$)|(^-(\.|,)\d{1,}$)/
+const isFloat = (REGEX?: RegExp) => value => value !== '-' && value !== '-.' && value !== '-,'
+  ? (REGEX ?? FLOAT_REGEX).test(value)
+  : true
+
+const SIGN_AND_SEPARATOR_REGEX = /(^-[,\.]$)/
+const SEPARATOR_REGEX = /(^[,\.]$)/
+const isSignedSeparator = v => SIGN_AND_SEPARATOR_REGEX.test(v)
+const isSeparator = v => SEPARATOR_REGEX.test(v)
+
+const stringifyValue = v => v === undefined && v === null ? '' : typeof v === 'string' ? v : `${v}`
+
+const isFunc = func => typeof func === 'function'
 
 export interface INumberInput extends Omit<InputProps, 'onChange' | 'onBlur'> {
-  int?: boolean,
-  positive?: boolean,
-  negative?: boolean,
-  value?: number | string,
-  onChange?: Function,
-  onBlur?: Function,
+  int?: boolean;
+  positive?: boolean;
+  negative?: boolean;
+  value?: number | string;
+  onChange?: Function;
+  onBlur?: Function;
+  min?: number;
+  max?: number;
+  onlyPositive?: boolean;
+  onlyNegative?: boolean;
+  shouldUpdate?: (currValue?: number | string | null, prevValue?: number | string | null) => boolean;
+  maxDigitsAfterPoint?: number;
+  maxExcluding?: boolean;
+  minExcluding?: boolean;
+  commaSeparator?: boolean;
+  cutZeroes?: boolean;
+  cutEndingZeroes?: boolean;
+  cutLeadingZeroes?: boolean;
+
 }
 
-const NumberInput: FC<INumberInput> = ({ int, positive, negative, value: _value, onChange, onBlur, ...props }) => {
-  const convert = v => !int ? parseFloat(v) : parseInt(v, 10)
+const NumberInput: FC<INumberInput> = ({
+  int,
+  negative,
+  onlyPositive,
+  onlyNegative,
+  shouldUpdate,
+  maxDigitsAfterPoint,
+  maxExcluding,
+  minExcluding,
+  commaSeparator,
+  min,
+  max,
+  value: _value,
+  onChange,
+  onBlur,
+  cutZeroes,
+  cutEndingZeroes,
+  cutLeadingZeroes,
+  ...props
+}) => {
+  const $input = useRef(null)
+  const formatToNumber = useCallback((v: string) => {
+    if (v.length > 1) {
+      if (
+        (cutZeroes || cutEndingZeroes) &&
+        (v.includes(',') || v.includes('.')) &&
+        v.endsWith('0')
+      ) {
+        return formatToNumber(v.slice(0, -1))
+      }
+      // 001
+      if (
+        (cutZeroes || cutLeadingZeroes) &&
+        v.startsWith('0')
+      ) {
+        return formatToNumber(v.slice(1))
+      }
+      // -0000 or -0 or -0001
+      if (
+        (cutZeroes || cutLeadingZeroes) &&
+        v.startsWith('-0')
+      ) {
+        const cutValue = v.slice(2)
+        return formatToNumber(!cutValue.length ? '0' : `-${v.slice(2)}`)
+      }
+    }
+    // 10.00 or 10,00
+    if (v === '-' || v === '-.' || v === '-,') {
+      return ''
+    }
+    // 123. or 123,
+    if (v.endsWith(',') || v.endsWith('.')) {
+      return v.slice(0, -1)
+    }
+    // ,123 or .123
+    if (v.startsWith(',') || v.startsWith('.')) {
+      return `0${v}`
+    }
+    // -,123 or -.123
+    if (v.startsWith('-,') || v.startsWith('-.')) {
+      return `-0${v.slice(1)}`
+    }
+
+    return v
+  }, [])
+
+  const validate = useMemo(() => {
+    if (isFunc(shouldUpdate)) return (curValue?: any, prevValue?: any): boolean => shouldUpdate(curValue, prevValue)
+  
+    const _isFloat = typeof maxDigitsAfterPoint === 'number' &&
+      !Number.isNaN(maxDigitsAfterPoint)
+      ? isFloat(
+        new RegExp(
+          FLOAT_REGEX.toString()
+            .replace(/,\)\\d\{1,\}/g, `,)\\d{1,${maxDigitsAfterPoint}}`)
+            .slice(1, -1)
+            .replace(/\//g, '//'),
+        ),
+      )
+      : isFloat()
+    const isPosFloat = value => _isFloat(value) && isPos(value)
+    const isNegFloat = value => value !== '-' ? _isFloat(value) && (isZero(value) || !isPos(value)) : true
+  
+    const withinMax = v => maxExcluding ? v < max : v <= max
+    const withinMin = v => (minExcluding && int) ? v > min : v >= min
+  
+    if (int) {
+      if (onlyPositive) return v => isPosInt(v) && withinMax(v) && withinMin(v)
+      if (onlyPositive) return v => (isNegInt(v) && withinMax(v) && withinMin(v)) || v === '-'
+      return v => (isInt(v) && withinMax(v) && withinMin(v)) || v === '-'
+    }
+  
+    if (onlyPositive) {
+      return (v) => {
+        const valueStr = stringifyValue(v).replace(',', '.')
+        return (isPosFloat(v) && withinMax(valueStr) && withinMin(valueStr)) || valueStr === '.'
+      }
+    }
+    if (onlyNegative) {
+      return (v) => {
+        const valueStr = stringifyValue(v).replace(',', '.')
+        return (
+          (isNegFloat(v) && withinMax(valueStr) && withinMin(valueStr)) ||
+          isSeparator(valueStr) ||
+          valueStr === '-' ||
+          isSignedSeparator(valueStr)
+        )
+      }
+    }
+  
+    return (v) => {
+      const valueStr = stringifyValue(v).replace(',', '.')
+      return (
+        (_isFloat(v) && withinMax(valueStr) && withinMin(valueStr)) ||
+        isSeparator(valueStr) ||
+        valueStr === '-' ||
+        isSignedSeparator(valueStr)
+      )
+    }
+  }, [ int, onlyPositive, onlyNegative, negative, shouldUpdate, maxDigitsAfterPoint, maxExcluding, minExcluding, min, max ])
 
   const [ value, setValue ] = useState(() => {
-    const v = convert(_value)
+    const v = formatToNumber(stringifyValue(_value))
     if (!Number.isNaN(v)) return v
     return ''
   })
 
-  const isValid = (v) => {
-    if (int) {
-      if (positive) return isPosInt(v)
-      if (negative) return isNegInt(v)
-      return isInt(v)
-    }
-    
-    if (positive) return isPosFloat(v)
-    if (negative) return isNegFloat(v)
-    return isFloat(v)
-  }
-
   const handleChange = (e) => {
-    let { value: v } = e.target
-    if (v === '' || isValid(v)) {
+    let v = e.target.value
+    if (v === '') {
       setValue(v)
-      if (typeof onChange === 'function') onChange(v, e)
-    } else {
-      setValue(value)
-      if (typeof onChange === 'function') onChange(value, e)
+      if (isFunc(onChange)) onChange(v, e)
+      return
     }
+
+    const element = $input.current.input
+    let caret = element.selectionStart
+
+    if (onlyNegative && !v.startsWith('-') && !v.startsWith('0')) {
+      v = `-${v}`
+      caret += 1
+    }
+
+    const isValid = isFunc(shouldUpdate)
+      ? validate(v, value)
+      : validate(v)
+
+    if (!isValid) {
+      requestAnimationFrame(() => {
+        element.selectionStart = caret - 1
+        element.selectionEnd = caret - 1
+      })
+      return
+    }
+
+    v = commaSeparator ? v.replace('.', ',') : v.replace(',', '.')
+
+    requestAnimationFrame(() => {
+      element.selectionStart = caret
+      element.selectionEnd = caret
+    })
+
+    setValue(v)
+    if (isFunc(onChange)) onChange(v, e)
   }
 
   const handleBlur = (e) => {
     let { value: v } = e.target
-    if (v === '' || isValid(v)) {
-      v = v ? convert(v) : v
+    if (v === '' || validate(v)) {
+      v = v ? formatToNumber(v) : v
       setValue(v)
       if (typeof onBlur === 'function') onBlur(v, e)
     } else {
@@ -68,17 +224,40 @@ const NumberInput: FC<INumberInput> = ({ int, positive, negative, value: _value,
   }
 
   useEffect(() => {
-    if (_value !== value) {
-      const v = convert(_value)
-      if (!Number.isNaN(v)) {
-        setValue(v)
-        return
+    let propValue = stringifyValue(_value)
+    const stateValue = stringifyValue(value)
+
+    if (propValue !== stateValue) {
+      if (propValue.replace(',', '.') !== stateValue.replace(',', '.')) {
+        if (propValue === '') {
+          setValue(propValue)
+          return
+        }
+
+        const isValid = isFunc(shouldUpdate)
+          ? validate(propValue, value)
+          : validate(propValue)
+
+        if (!isValid) {
+          return
+        }
+
+        propValue = commaSeparator
+          ? propValue.replace('.', ',')
+          : propValue
+        setValue(propValue)
       }
-      setValue('')
     }
   }, [ _value ])
 
-  return <Input value={value} onChange={handleChange} onBlur={handleBlur} {...props} />
+  return <Input ref={$input} value={value} onChange={handleChange} onBlur={handleBlur} {...props} />
+}
+
+NumberInput.defaultProps = {
+  min: -Infinity,
+  max: Infinity,
+  cutEndingZeroes: true,
+  cutLeadingZeroes: true,
 }
 
 export default NumberInput
